290b159675a0ad38cdef064cbf1712f822b3b929
MashaArchipenko <masha.archipenko.99@mail.ru>
2021-02-25 16:51:05 +0300

    finished start page

--- react-project/src/Components/SignIn.jsx
+++ react-project/src/Components/SignIn.jsx
@@ -1,17 +1,37 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Form} from "react-bootstrap";
 
+export default class SignIn extends Component {
+render() {
+    return (
+    <>
+        <div className="form">
+            <span>Sign In</span>
+        <Form>
+            <Form.Group>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
 
-export default class SignIn extends Component{
-    render()
-    {
-        return(
-            <div className="notSignUpBlock">
-                <div className="textSignUp">
-                <p>Don't have account?</p>
-                <Link to="/signOn">Sign Up</Link>
-                </div>
-            </div>
-        );
-    }
+            <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <button className="button">
+            Submit
+            </button>
+        </Form>
+        </div>
+        <div className="changeBlock">
+        <div className="textSignUp">
+            <p>Don't have account?</p>
+            <Link to="/signOn" className="button">
+            Sign Up
+            </Link>
+        </div>
+        </div>
+    </>
+    );
+}
 }
--- react-project/src/Components/SignOn.jsx
+++ react-project/src/Components/SignOn.jsx
@@ -1,17 +1,38 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Form} from "react-bootstrap";
 
 export default class SignOn extends Component
 {
     render()
     {
         return(
-            <><div className="signOnForm">
+            <>
+            <div className="form">
+            <span>Sign On</span>
+        <Form>
+        <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="name" placeholder="Enter name" />
+            </Form.Group>
+            <Form.Group>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <button className="button">
+            Submit
+            </button>
+        </Form>
             </div>
-            <div className="notSignUpBlock">
+            <div className="changeBlock">
                 <div className="textSignUp">
-                <p>Войдите в аккаунт</p>
-                <Link to="/">Sign In</Link>
+                <p>Sign in to your account</p>
+                <Link to="/" className="button">Sign In</Link>
                 </div>
             </div>
             
