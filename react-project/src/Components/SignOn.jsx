555c07c4b0d030c7b6a668f0510396136f7a1d1f
MashaArchipenko <masha.archipenko.99@mail.ru>
2021-02-28 14:42:11 +0300

    rrr

--- react-project/src/Components/SignOn.jsx
+++ react-project/src/Components/SignOn.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Form } from "react-bootstrap";
-import {Link} from 'react-router-dom';
+import {Link} from "react-router-dom";
 
-const SignIn = (props) => {
+const SignOn = (props) => {
   const {
     email,
     setEmail,
     password,
     setPassword,
-    handleLogin,
+    handleSignUp,
     emailError,
     passwordError,
   } = props;
@@ -16,7 +16,7 @@ const SignIn = (props) => {
   return (
     <>
       <div className="form">
-      <span>Sign In</span>
+        <span>Sign On</span>
         <Form>
           <Form.Group>
             <Form.Label>Email address</Form.Label>
@@ -24,7 +24,7 @@ const SignIn = (props) => {
               type="email"
               value={email}
               required
-              onChange={(e=> setEmail(e.target.value))}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Enter email"
             />
             <p className="errorMsg">{emailError}</p>
@@ -36,24 +36,28 @@ const SignIn = (props) => {
               type="password"
               required
               value={password}
-              onChange={(e => setPassword(e.target.value))}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
             />
             <p className="errorMsg">{passwordError}</p>
           </Form.Group>
-          <div className="btnContainer"> 
-            <button className="button"  onClick={handleLogin}>Sign in</button>
-        </div>
+          <div className="btnContainer">
+            <button onClick={handleSignUp} className="button">
+              Sign On
+            </button>
+          </div>
         </Form>
       </div>
       <div className="changeBlock">
         <div className="textSignUp">
-          <p>Don't have account?</p>
-          <Link to="/signOn" className="button"> Sign On</Link>
-        </div> 
+          <p>Sign in to your account</p>
+          <Link to="/" className="button">
+            Sign In
+          </Link>
+        </div>
       </div>
     </>
   );
 };
 
-export default SignIn;
+export default SignOn;
